36cba765826fe6a59615f93d96ba9a57c911c308
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2023-09-21 21:21:48 +0100

    solve for mutex lock cpp
    - added an if condition to check current thread against total - 1
    - added semaphore lock around the critical section
    - added a signal inside the branch if threads were n - 1 to start a
      chain reaction

--- lab1/original/mutualEx.cpp
+++ lab1/original/mutualEx.cpp
@@ -16,24 +16,33 @@ int sharedVariable=0;
 */
 /*! displays a message that is split in to 2 sections to show how a rendezvous works*/
 void updateTask(std::shared_ptr<Semaphore> firstSem, int numUpdates){
-
  
+  firstSem->Wait(); // The initial signal will be called
   for(int i=0;i<numUpdates;i++){
-    //UPDATE SHARED VARIABLE HERE!
     sharedVariable++;
   }
-
+  // After the first signal is called and we have processed the first thread,
+  // we signal for the next thread to start
+  firstSem->Signal();
 }
 
 
 int main(void){
   std::vector<std::thread> vt(num_threads);
-  std::shared_ptr<Semaphore> aSemaphore( new Semaphore);
+  std::shared_ptr<Semaphore> aSemaphore(new Semaphore);
+  std::shared_ptr<Semaphore> bSemaphore(new Semaphore);
   /**< Launch the threads  */
   int i=0;
-  for(std::thread& t: vt){
+  for(std::thread& t: vt) { 
     t=std::thread(updateTask,aSemaphore,1000);
-  }
+    i++;
+    if(i == (num_threads - 1)) {
+	// The last thread has been reached
+	// This will signal one thread to start, which will then signal
+	// the other threads in an almost recursive fashion
+	aSemaphore->Signal();
+    }
+  }	    
   std::cout << "Launched from the main\n";
   /**< Join the threads with the main thread */
   for (auto& v :vt){
