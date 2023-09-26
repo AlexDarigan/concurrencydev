f3f9f778eefc7f7e5037e75166fff4cba8804178
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2023-09-26 10:14:28 +0100

    use shared ptr for barrier instead of static int

--- labTwo/rendezvous.cpp
+++ labTwo/rendezvous.cpp
@@ -4,18 +4,16 @@
 #include <iostream>
 
 /*! displays the first function in the barrier being executed */
-void task(std::shared_ptr<Semaphore> mutexSem,std::shared_ptr<Semaphore> barrierSem, int threadCount){
+void task(std::shared_ptr<Semaphore> mutexSem,std::shared_ptr<Semaphore> barrierSem, std::shared_ptr<int> threadCount){
 
   mutexSem->Wait();     	
   std::cout << "first" << std::endl;
   mutexSem->Signal();
 
-  static int barrier;
-  barrier++;
-  if(barrier < threadCount) {
+  (*threadCount)--;
+  if((*threadCount) > 0) {
 	  barrierSem->Wait();
   }
-  barrier = 0;
   barrierSem->Signal();
   
   mutexSem->Wait();
@@ -26,15 +24,18 @@ void task(std::shared_ptr<Semaphore> mutexSem,std::shared_ptr<Semaphore> barrier
 int main(void){
   std::shared_ptr<Semaphore> mutexSem;
   std::shared_ptr<Semaphore> barrierSem;
+  std::shared_ptr<int> barrier;
   int threadCount = 5;
   mutexSem=std::make_shared<Semaphore>(1);
   barrierSem=std::make_shared<Semaphore>(0);
   /*!< An array of threads*/
   std::vector<std::thread> threadArray(threadCount);
   /*!< Pointer to barrier object*/
+  barrier = std::make_shared<int>(threadCount);
+
 
   for(int i=0; i < threadArray.size(); i++){
-    threadArray[i]=std::thread(task,mutexSem,barrierSem,threadCount);
+    threadArray[i]=std::thread(task,mutexSem,barrierSem,barrier);
   }
 
   for(int i = 0; i < threadArray.size(); i++){
