3a1523343e297c0b376831e903eb64a94f59d2bd
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2023-10-09 12:34:26 +0100

    add simple test framework

---
+++ testlib/asserts.h
@@ -0,0 +1,6 @@
+template <typename A, typename B>
+void isEqual(A a, B b) {
+        bool result = (a == b);
+        std::cout << result << std::endl;
+}
+
---
+++ testlib/test.cpp
@@ -0,0 +1,5 @@
+#include "asserts.h"
+
+int main() {
+	isEqual<int,int>(20,10);
+}
