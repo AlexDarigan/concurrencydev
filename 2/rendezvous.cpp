87ff7e7ae30247831480d3a36650d64bc27376d5
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2023-10-11 00:00:39 +0100

    reimplement manual test

--- 2/rendezvous.cpp
+++ 2/rendezvous.cpp
@@ -73,24 +73,24 @@ void testCounts() {
 
 int main(void){
   // Manual Test
-  // std::shared_ptr<Semaphore> mutexSem;
-  // std::shared_ptr<Semaphore> barrierSem;
-  // std::shared_ptr<int> barrier;
-  // int threadCount = 5;
-  // mutexSem=std::make_shared<Semaphore>(1);
-  // barrierSem=std::make_shared<Semaphore>(0);
-  // /*!< An array of threads*/
-  // std::vector<std::thread> threadArray(threadCount);
-  // /*!< Pointer to barrier object*/
-  // barrier = std::make_shared<int>(threadCount);
+  std::shared_ptr<Semaphore> mutexSem;
+  std::shared_ptr<Semaphore> barrierSem;
+  std::shared_ptr<int> barrier;
+  int threadCount = 5;
+  mutexSem=std::make_shared<Semaphore>(1);
+  barrierSem=std::make_shared<Semaphore>(0);
+  /*!< An array of threads*/
+  std::vector<std::thread> threadArray(threadCount);
+  /*!< Pointer to barrier object*/
+  barrier = std::make_shared<int>(threadCount);
 
-  // for(int i=0; i < threadArray.size(); i++){
-  //   threadArray[i]=std::thread(task,mutexSem,barrierSem,barrier);
-  // }
+  for(int i=0; i < threadArray.size(); i++){
+    threadArray[i]=std::thread(task,mutexSem,barrierSem,barrier);
+  }
 
-  // for(int i = 0; i < threadArray.size(); i++){
-  //   threadArray[i].join();
-  // }
+  for(int i = 0; i < threadArray.size(); i++){
+    threadArray[i].join();
+  }
 
   // Other tests
   testCounts();
