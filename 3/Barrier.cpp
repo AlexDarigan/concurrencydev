e4fbb24661a735bfb2890b375a2cb2cb43aa127c
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2023-11-22 13:27:18 +0000

    apply stash

--- 3/Barrier.cpp
+++ 3/Barrier.cpp
@@ -14,20 +14,10 @@ Barrier::Barrier(){
   this->count = 0;
   threadNum = 0;
   condition = false;
-<<<<<<< Updated upstream
-  mutex = std::make_shared<Semaphore>(1);
-  entry = std::make_shared<Semaphore>(0);
-  exit = std::make_shared<Semaphore>(1);
-
-=======
   mutex=std::make_shared<Semaphore>(1);
-  //std::shared_ptr<Semaphore> mutex(new Semaphore(1));
   barrier1=std::make_shared<Semaphore>(0);
-  //std::shared_ptr<Semaphore> barrier1(new Semaphore(0));
-  //std::shared_ptr<Semaphore> barrier2(new Semaphore(1));
   entry=std::make_shared<Semaphore>(0);
   exit=std::make_shared<Semaphore>(1);
->>>>>>> Stashed changes
 }
 
 /*! Barrier with parameter constructor*/
@@ -36,17 +26,10 @@ Barrier::Barrier(int count){
   this->count = count;
   threadNum = 0;
   condition = false;
-<<<<<<< Updated upstream
-  mutex = std::make_shared<Semaphore>(1);
-  entry = std::make_shared<Semaphore>(0);
-  exit = std::make_shared<Semaphore>(1);
-=======
   mutex=std::make_shared<Semaphore>(1);
   entry=std::make_shared<Semaphore>(0);
   exit=std::make_shared<Semaphore>(1);
-  //std::shared_ptr<Semaphore> barrier1(new Semaphore(0));
-  // std::shared_ptr<Semaphore> barrier2(new Semaphore(1));
->>>>>>> Stashed changes
+
 }
 /*! Barrier deconstructor*/
 Barrier::~Barrier(){
@@ -60,24 +43,6 @@ void Barrier::waitForAll(){
   mutex->Wait();
   threadNum++;
 
-<<<<<<< Updated upstream
-  if(threadNum == count){
-    exit->Wait(); // 0
-    entry->Signal(); // 1
-  }
-  mutex->Signal();
-  entry->Wait();
-  entry->Signal();
-  mutex->Wait();
-  threadNum--;
-  if(threadNum == 0) {
-    entry->Wait(); // -count
-    exit->Signal(); // 1
-  }
-  mutex->Signal();
-  exit->Wait();
-  exit->Signal();
-=======
   if(threadNum == count) {
     entry->Signal();
     threadNum = 0;
@@ -85,7 +50,6 @@ void Barrier::waitForAll(){
   mutex->Signal(); //
   entry->Wait();
   entry->Signal();
->>>>>>> Stashed changes
 }
 
 
